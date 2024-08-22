29b512bec238221266a220f022c45e4601dae484
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-08-22 13:56:23 -0300

    Implementadas views

---
+++ LocadoraDeVeiculos.WebApp/Mapping/VeiculoProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
+using LocadoraDeVeiculos.WebApp.Models;
+
+namespace LocadoraDeVeiculos.WebApp.Mapping;
+
+public class VeiculoProfile : Profile
+{
+    public VeiculoProfile()
+    {
+        CreateMap<InserirVeiculoViewModel, Veiculo>();
+        CreateMap<EditarVeiculoViewModel, Veiculo>();
+
+        CreateMap<Veiculo, ListarVeiculoViewModel>()
+            .ForMember(
+                dest => dest.GrupoVeiculos,
+                opt => opt.MapFrom(src => src.GrupoVeiculos!.Nome)
+            );
+
+        CreateMap<Veiculo, DetalhesVeiculoViewModel>()
+            .ForMember(
+                dest => dest.GrupoVeiculos,
+                opt => opt.MapFrom(src => src.GrupoVeiculos!.Nome)
+            );
+
+        CreateMap<Veiculo, EditarVeiculoViewModel>();
+    }
+}
